b2743cc965f356f99d598a45a328812ae0223ac3
nisha <nishakulkarni2006@gmail.com>
2024-09-15 08:44:49 +0530

    added related products

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'> 
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+            An e-commerce website is an online platform that facilitate buying and selling 
+            of products or services over the internet serves as a virtual marketplace whre business
+            and individual showcase their products , interact with customers and conducted tracsactions
+            without the need for a physical presence. E-commerce websites have gained immense popularity
+            due to their convntion accesssibility , and the global reach they offer. 
+        </p>
+        <p>
+            An e-commerce website is an online platform that facilitate buying and selling 
+            of products or services over the internet serves as a virtual marketplace whre business
+            and individual showcase their products , interact with customers and conducted tracsactions
+            without the need for a physical presence. E-commerce websites have gained immense popularity
+            due to their convntion accesssibility , and the global reach they offer. 
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox;
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,38 +1,14 @@
-// import React, { useContext, useState } from 'react'
-// import { ShopContext } from '../Context/ShopContext';
-// import { useParams } from 'react-router-dom';
-// import Breadcrumbs from '../Components/Breardcrumbs/Breadcrumbs';
-// import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
-
-// const Product = () => {
-//   const {all_product} = useContext(ShopContext);
-//   const {productId} = useParams(); 
-//   const product = all_product.find((e)=>e.id === Number(productId));
-//   return (
-//     <div>
-//       <Breadcrumbs product={product}/>
-//       <ProductDisplay products= {product}/>
-//     </div>
-//   )
-// }
-
-// export default Product;
-
-
 import React, { useContext } from 'react';
 import { ShopContext } from '../Context/ShopContext';
 import { useParams } from 'react-router-dom';
 import Breadcrumbs from '../Components/Breadcrumbs/Breadcrumbs';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProduct from '../Components/RelatedProducts/RelatedProduct';
 
 const Product = () => {
-  // Use useContext to get the data from ShopContext
   const { all_product } = useContext(ShopContext);
-  
-  // Extract the productId from URL parameters
   const { productId } = useParams(); 
-
-  // Find the product based on the productId
   const product = all_product.find((e) => e.id === Number(productId));
 
   return (
@@ -40,7 +16,8 @@ const Product = () => {
       {/* Pass the product data to the Breadcrumbs component */}
       <Breadcrumbs product={product} />
        <ProductDisplay products= {product}/>
-
+        <DescriptionBox/>
+        <RelatedProduct/>
     </div>
   );
 }
