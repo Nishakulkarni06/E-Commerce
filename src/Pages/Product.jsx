8c2b29b4b240569a7f40d516cb67c9a3f70e9993
nisha <nishakulkarni2006@gmail.com>
2024-09-15 07:59:15 +0530

    added click functionality to photos

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+// import React, { useContext, useState } from 'react'
+// import { ShopContext } from '../Context/ShopContext';
+// import { useParams } from 'react-router-dom';
+// import Breadcrumbs from '../Components/Breardcrumbs/Breadcrumbs';
+// import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+
+// const Product = () => {
+//   const {all_product} = useContext(ShopContext);
+//   const {productId} = useParams(); 
+//   const product = all_product.find((e)=>e.id === Number(productId));
+//   return (
+//     <div>
+//       <Breadcrumbs product={product}/>
+//       <ProductDisplay products= {product}/>
+//     </div>
+//   )
+// }
+
+// export default Product;
+
+
+import React, { useContext } from 'react';
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrumbs from '../Components/Breadcrumbs/Breadcrumbs';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+  // Use useContext to get the data from ShopContext
+  const { all_product } = useContext(ShopContext);
+  
+  // Extract the productId from URL parameters
+  const { productId } = useParams(); 
+
+  // Find the product based on the productId
+  const product = all_product.find((e) => e.id === Number(productId));
+
   return (
     <div>
-      
+      {/* Pass the product data to the Breadcrumbs component */}
+      <Breadcrumbs product={product} />
+       <ProductDisplay products= {product}/>
+
     </div>
-  )
+  );
 }
 
 export default Product;
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import Item from '../Components/Items/item.jsx' 
+import all_product from '../Components/Assets/all_product'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const {all_Product} = useContext(ShopContext);
   return (
-    <div>
-      
+    <div className='shop-category'>
+      <img className='shopCategory-banner' src={props.banner}/> 
+      <div className="shopcategory-indexSort">
+        <p>
+          <span> Showing 1-12 </span> out of 36 products
+        </p>
+       <div className="shopcategory-sort">
+        Sort by <img src={dropdown_icon} alt=''/>
+       </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product.map((item,i)=>{
+          if(props.category === item.category){
+            return <Item key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+          }
+          else{
+            return null;
+          }
+        })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore More
+      </div>
     </div>
   )
 }
